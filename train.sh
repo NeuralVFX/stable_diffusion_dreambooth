1645840db4ca8d2e40b5cde9749759444f3558b7
neuralvfx <neuralvfx@gmail.com>
2023-05-15 01:48:25 +0000

    simple workspace for training dreambooth

---
+++ train.sh
@@ -0,0 +1,21 @@
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="./booth_dataset"
+export CLASS_DIR="./class_dataset"
+export OUTPUT_DIR="./out"
+
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --class_data_dir=$CLASS_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --instance_prompt="a man mrbeast" \
+  --class_prompt="a man" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=200 \
+  --max_train_steps=800 \
